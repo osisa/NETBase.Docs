97b38d9e5e448c180f5089fa878ff604c2c8365c
OserRemo <remo.oser@osisa.com>
2022-10-11 10:19:42 +0200

    test start

--- src/osisa.Docs.Tests/CodeQueryTests.cs
+++ src/osisa.Docs.Tests/CodeQueryTests.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using Moq;
+
+using osisa.Docs.Tests.TestInfrastructure;
 
 using Shouldly;
 
@@ -16,6 +19,8 @@ using Statiq.Core;
 using Statiq.Docs;
 using Statiq.Testing;
 
+using static osisa.Docs.Tests.TestInfrastructure.TestValues;
+
 #pragma warning disable SA1300 // Element should begin with upper-case letter
 namespace osisa.Docs.Tests
 #pragma warning restore SA1300 // Element should begin with upper-case letter
@@ -23,14 +28,64 @@ namespace osisa.Docs.Tests
     [TestClass]
     public class CodeQueryTests
     {
+        /// <summary>
+        /// Gets or sets the test context.
+        /// </summary>
+        // ReSharper disable once UnusedMember.Global
+        // ReSharper disable once MemberCanBePrivate.Global
+        // ReSharper disable once UnusedAutoPropertyAccessor.Global
+        public TestContext TestContext { get; set; }
+
+        /////async Task GetCodeFilesAsync()
         [TestMethod]
-        public void GetCodeFiles()
+        public async Task GetCodeFilesAsync()
         {
             // Arrange
-            IExecutionContext ctx = new Mock<IExecutionContext>().Object;
+            Bootstrapper bootstrapper = Bootstrapper
+                .Factory
+                .Create(Array.Empty<string>())
+                .SetRootPath(TestPath);
 
             // Act
-            AnalyzeCSharp result = new AnalyzeCSharp()
+            BootstrapperTestResult result = await bootstrapper.RunTestAsync(LogLevel.Information);
+
+            // Assert
+            result.ExitCode.ShouldBe(0);
+            result.Outputs.Count.ShouldBe(0);
+            result.ShouldNotBeNull();
+            bootstrapper.FileSystem.ShouldNotBeNull();
+            TestContext.WriteLine("Files: {0}", bootstrapper.FileSystem.RootPath);
+            bootstrapper.FileSystem.RootPath.ShouldBe(TestPath);
+            TestContext.WriteLine("Output: {0}", bootstrapper.FileSystem.OutputPath);
+        }
+
+        [TestMethod]
+        public async Task GetCodeFiles3Async()
+        {
+            // Arrange
+            Bootstrapper bootstrapper = Bootstrapper
+                .Factory
+                .CreateDefault(Array.Empty<string>())
+                .BuildPipeline("test", pipeline => pipeline
+                    .WithInputModules(new ReadFiles("**/*.*"))
+                    .WithProcessModules(new WriteFiles()))
+                    ////.WithOutputModules(new WriteFiles()))
+                .SetRootPath(TestPath);
+
+            // Act
+            int result = await bootstrapper.RunAsync();
+
+            // Assert
+            result.ShouldBe(0);
+        }
+
+        [TestMethod]
+        public async Task GetCodeFiles2Async()
+        {
+            // Arrange
+            Bootstrapper xx = Bootstrapper.Factory.Create(Array.Empty<string>()).SetRootPath(@"c:\TestBin");
+            IExecutionContext ctx = new Mock<IExecutionContext>().Object;
+            AnalyzeCSharp analyzer = new AnalyzeCSharp()
                 .WhereNamespaces(ctx.Settings.GetBool(DocsKeys.IncludeGlobalNamespace))
                 .WherePublic()
                 .WithCssClasses("code", "cs")
@@ -41,8 +96,13 @@ namespace osisa.Docs.Tests
                 .WithAssemblySymbols()
                 .WithImplicitInheritDoc(ctx.GetBool(DocsKeys.ImplicitInheritDoc));
 
+            // Act
+            IEnumerable<IDocument> result1 = await analyzer.ExecuteAsync(ctx);
+            IDocument[] result = result1.ToArray();
+
             // Assert
             result.ShouldNotBeNull();
+            result.Length.ShouldBe(1);
         }
 
         [TestMethod]
---
+++ src/osisa.Docs.Tests/TestInfrastructure/TestValues.cs
@@ -0,0 +1,9 @@
+﻿#pragma warning disable SA1300 // Element should begin with upper-case letter
+namespace osisa.Docs.Tests.TestInfrastructure
+#pragma warning restore SA1300 // Element should begin with upper-case letter
+{
+    internal static class TestValues
+    {
+        public const string TestPath = @"c:\TestBin\";
+    }
+}
